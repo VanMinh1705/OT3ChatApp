1c86d0cbcc90949254a5fbf052d4036d6362aac4
VanMinh1705 <huynhvminhofficial@gmail.com>
2024-01-19 15:53:14 +0700

    update screen ChatSceen

--- O3ChatApp/screen/Inside/ChatSceen.js
+++ O3ChatApp/screen/Inside/ChatSceen.js
@@ -1,16 +1,81 @@
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+} from "react-native";
 import React from "react";
+import { Dimensions } from "react-native";
+import IconAnt from "react-native-vector-icons/AntDesign";
+import { useFonts } from "expo-font";
+import { LinearGradient } from "expo-linear-gradient";
+
+export const { width: WINDOW_WIDTH, height: WINDOW_HEIGHT } =
+  Dimensions.get("window");
 
 const ChatSceen = () => {
+  const [fontsLoaded] = useFonts({
+    "keaniaone-regular": require("../../assets/fonts/KeaniaOne-Regular.ttf"),
+  });
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.header}></View>
-      <Text
-        onPress={() => alert('This is the "Home" screen.')}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Home Screen
-      </Text>
+      <StatusBar barStyle="light-content" backgroundColor={"#1197bd"} />
+
+      <SafeAreaView>
+        <View style={styles.upperHeaderPlaceholer} />
+      </SafeAreaView>
+
+      <SafeAreaView style={styles.header}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-around",
+            marginTop: 10,
+          }}
+        >
+          <IconAnt name="search1" size={30} color={"#fff"} />
+          <TextInput
+            placeholder="Tìm kiếm"
+            placeholderTextColor={"#fff"}
+            style={{
+              width: 235,
+              height: 30,
+              color: "#000", // This is the text color when there is input
+              fontSize: 16,
+              borderRadius: 10,
+              paddingLeft: 10,
+              borderWidth: 1,
+            }}
+          />
+          <IconAnt name="qrcode" size={30} color={"#fff"} />
+          <IconAnt name="plus" size={30} color={"#fff"} />
+        </View>
+        <View style={styles.logo}>
+          <Text style={styles.txtLogo}>4MChat</Text>
+        </View>
+        <View style={styles.upperHeader} />
+
+        <View style={styles.lowerHeader} />
+      </SafeAreaView>
+      <ScrollView>
+        <View style={styles.paddingForHeader} />
+        <View style={styles.viewContent}>
+          <LinearGradient
+            // Background Linear Gradient
+            colors={["#4AD8C7", "#B728A9"]}
+            style={styles.background}
+          />
+          <Text>Xin chaopf</Text>
+        </View>
+        <View style={styles.scrollViewContent} />
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -22,9 +87,49 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff",
   },
+  background: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+  },
   header: {
-    width: 375,
-    height: 57,
+    width: "100%",
     backgroundColor: "#03c6fc",
+    position: "absolute",
+  },
+  paddingForHeader: {
+    height: 50,
+  },
+  upperHeaderPlaceholer: {
+    height: 50,
+  },
+  upperHeader: {
+    height: 50,
+  },
+  lowerHeader: {
+    height: 50,
+  },
+  scrollViewContent: {
+    height: WINDOW_HEIGHT,
+    backgroundColor: "white",
+  },
+  viewContent: {
+    width: "100%",
+    height: "100%",
+    backgroundColor: "#fff",
+  },
+  txtLogo: {
+    color: "#fff",
+    fontSize: 30,
+    fontFamily: "keaniaone-regular",
+  },
+  logo: {
+    width: 120,
+    alignItems: "center",
+    height: 40,
+    borderRadius: 10,
+    backgroundColor: "rgba(217, 217, 217, 0.50)",
+    alignSelf: "center",
+    marginTop: 10,
   },
 });
