1d494355450929b7b32a7a39863e644dec684bde
VanMinh1705 <huynhvminhofficial@gmail.com>
2024-01-20 16:51:08 +0700

    update PhoneBook

---
+++ O3ChatApp/screen/Inside/FriendScreen.js
@@ -0,0 +1,17 @@
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FriendScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>FriendScreen</Text>
+      <View style={{ backgroundColor: "red" }}>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default FriendScreen;
+
+const styles = StyleSheet.create({});
--- O3ChatApp/screen/Inside/PhoneBookScreen.js
+++ O3ChatApp/screen/Inside/PhoneBookScreen.js
@@ -1,16 +1,51 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import FriendScreen from "./FriendScreen";
+import ChatSceen from "./ChatSceen";
 
 const PhoneBookScreen = () => {
+  const [select, setSelect] = useState(1);
+
+  const renderScreen = () => {
+    if (select === 1) {
+      return <FriendScreen />;
+    } else if (select === 2) {
+      return <ChatSceen />;
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
+    <SafeAreaView style={{ flex: 1, alignItems: "center" }}>
+      <View
+        style={{
+          width: "100%",
+          height: 50,
+          alignItems: "center",
+          backgroundColor: "#ccc",
+          flexDirection: "row",
+          justifyContent: "space-around",
+        }}
       >
-        Trang danh bạ
-      </Text>
-    </View>
+        <Pressable
+          onPress={() => {
+            setSelect(1);
+          }}
+          style={{}}
+        >
+          <Text>Bạn bè</Text>
+        </Pressable>
+        <Pressable
+          onPress={() => {
+            setSelect(2);
+          }}
+          style={{}}
+        >
+          <Text>Nhóm</Text>
+        </Pressable>
+      </View>
+
+      <SafeAreaView>{renderScreen()}</SafeAreaView>
+    </SafeAreaView>
   );
 };
 
